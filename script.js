12795a2a61b6f0898d7de1c67c804845d2c804b7
John <johnlouisstephenson@gmail.com>
2024-04-11 11:39:03 +0100

    Updated index.html & script.js

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-const annual = document.getElementById("annual");
-const monthly = document.getElementById("monthly");
+const buttons = document.querySelectorAll(".a-mBtn");
 const sums = document.querySelectorAll(".sum");
-let annualBool = false;
-let monthlyBool = true;
+const MONTHLY_LIMIT = 40;
 
-// Create an array from the sums
-const sumsArr = Array.from(sums);
-console.log(sumsArr);
-
-// Create function for handleAnnual
-function handleAnnual(sumsArr) {
-  if ((annualBool = true)) {
-    sumsArr.forEach((sum) => {
-      let tmp = parseFloat(sum.innerText);
-      if (tmp < 40) {
-        tmp *= 11;
-        console.log(tmp);
-        sum.innerText = parseFloat(tmp.toFixed(2));
-      } else {
-        return;
-      }
-    });
+// Function to handle annual activation
+function handleAnnual() {
+  if (parseFloat(sums[0].innerText) < 40) {
+    for (let i = 0; i < sums.length; i++) {
+      sums[i].innerText = parseFloat(sums[i].innerText * 11).toFixed(2);
+    }
   } else {
     return;
   }
 }
 
-// Function to handle monthly clicks
-function handleMonthly(sumsArr) {
-  if ((monthlyBool = true)) {
-    sumsArr[0].innerText = "19.99";
-    sumsArr[1].innerText = "24.99";
-    sumsArr[2].innerText = "39.99";
-  } else {
+// Function to handle monthly activation
+function handleMonthly() {
+  if (sums[0].innerText === "19.99") {
     return;
+  } else {
+    sums[0].innerText = "19.99";
+    sums[1].innerText = "24.99";
+    sums[2].innerText = "39.99";
   }
 }
 
-// Click event listener for annual
-annual.addEventListener("click", function () {
-  annualBool = true;
-  monthlyBool = false;
-  handleAnnual(sumsArr);
-});
-
-//Click event listener for monthly
-monthly.addEventListener("click", function () {
-  annualBool = false;
-  monthlyBool = true;
-  handleMonthly(sumsArr);
-});
-
+//Add eventlistener to both buttons
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (event) => {
+    if (event.target.innerText === "Annually") {
+      handleAnnual();
+    } else {
+      handleMonthly();
+    }
+  });
+}
